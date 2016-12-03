dc1e94d921456af1f74b59c7ed784cb7a96ea04f
srnux <srnux@hotmail.com>
2016-12-03 16:20:24 +0100

    using a fake API service and adding methods

--- app/hero.component.ts
+++ app/hero.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-heroes',
-  templateUrl: 'heroes.component.html',
-  styleUrls: ['heroes.component.css'], 
-  providers:[HeroService]
+  templateUrl: 'hero.component.html',
+  styleUrls: ['hero.component.css']
 })
 
 export class HeroesComponent implements OnInit {
-  title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
 
@@ -34,6 +34,25 @@ export class HeroesComponent implements OnInit {
   
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+  this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 
 }
